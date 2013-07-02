330fd0bdf08c9bccf517eddde229a40087464bcc
Alex WEb <plcgi1@gmail.com>
2013-07-02 03:15:21 +0300

    add photo upload and set edit-view buttons active

---
+++ public/app/scripts/services/settings.js
@@ -0,0 +1,10 @@
+'use strict';
+//angular.module('ahsServices', ['ngResource'])
+var app = window.app;
+app.services
+	.factory('Settings', function($resource){
+		return $resource('/ahs/settings', {}, {
+			query: {method:'GET', isArray:false}
+		}
+	);
+});
